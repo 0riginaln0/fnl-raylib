215fc0aaee1b95a7c59366f03e62ff0a61510a82
Ryzh <gooddmitriy@mail.ru>
2024-12-07 17:40:18 +0300

    cleanup and license

--- bindings_gen.py
+++ bindings_gen.py
@@ -31,45 +31,7 @@ def add_export_name(newname):
     export_names.append(f" : {newname}\n")
 
 
-# ignorelist = [
-#     # "// Callbacks to hook some internal functions",
-#     # "// WARNING: These callbacks are intended for advanced users",
-#     # "typedef void (*TraceLogCallback)(int logLevel, const char *text, va_list args);  // Logging: Redirect trace log messages",
-#     # "typedef unsigned char *(*LoadFileDataCallback)(const char *fileName, int *dataSize);    // FileIO: Load binary data",
-#     # "typedef bool (*SaveFileDataCallback)(const char *fileName, void *data, int dataSize);   // FileIO: Save binary data",
-#     # "typedef char *(*LoadFileTextCallback)(const char *fileName);            // FileIO: Load text data",
-#     # "typedef bool (*SaveFileTextCallback)(const char *fileName, char *text); // FileIO: Save text data",
-#     # "typedef void (*AudioCallback)(void *bufferData, unsigned int frames);",
-# ]
-
-# # Писать определения структур в ffi_cdef
-# # Писать функции-конструкторы для структур
-# if line.startswith("typedef"):
-
-#     pass
-# if line == "// Enumerators Definition":
-#     # Писать локальные переменные
-#     # Енамы могут быть как полностью определённые, так и только для первого элемен
-#     pass
-# if line == "// Global Variables Definition":
-#     # Начало
-#     pass
-# if line == "// Window and Graphics Device Functions (Module: core)":
-#     # Писать определения функций в ffi_cdef
-#     # Писать обёртки фнльные
-
-#     # Отделять блоки функций комментариями // Window-related functions
-#     # Комментарии перед блоками функций могут быть многострочными
-#     pass
-
-
 state = "comment"
-# +"comment" - запоминать комментарии (верхнего уровня). Записать при переходе в другой стейт
-# +"typedef struct" - записать определение структуры в cffistruct
-#                    записать функцию-конструктор для струкруты в construct
-# +"typedef enum"
-# +"typedef alias"
-# +"func"
 comments: list[str] = []
 cffistruct: list[str] = ['(ffi.cdef "\n']
 constructs: list[str] = []
